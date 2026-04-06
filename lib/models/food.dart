34f783191252d2267716e267bef37f8ca3733b41
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-04-06 18:37:26 +0630

    initial commit

---
+++ lib/models/food.dart
@@ -0,0 +1,11 @@
+class Food {
+  final  String imageUrl;
+  final  String name;
+  final String price;
+
+  Food({
+    required this.imageUrl,
+    required this.name,
+    required this.price,
+  });
+}
