500efb50971591cf9bf27795942550b3f760d3e1
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-04-11 15:25:52 +0630

    nearby restaurant list view done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery_ui_tutorial/data/data.dart';
+import 'package:food_delivery_ui_tutorial/models/restaurant.dart';
+import 'package:food_delivery_ui_tutorial/widgets/recent_orders.dart';
+
+import '../widgets/rating_stars.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +14,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  _buildRestaurants() {
+    List<Widget> _restaurantList = [];
+
+    restaurants.forEach((Restaurant  restaurant){
+      _restaurantList.add(Container(
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(width: 1.0, color: Colors.grey[200]!)
+        ),
+        child: Row(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                  width: 150.0,
+                  height: 150.0,
+                  image: AssetImage(restaurant.imageUrl),
+                  fit: BoxFit.cover
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(restaurant.name, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis),
+                    SizedBox(height: 4.0,),
+                    RatingStars(restaurant.rating),
+                    SizedBox(height: 4.0,),
+                    Text(restaurant.address, style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600), overflow: TextOverflow.ellipsis),
+                    SizedBox(height: 4.0,),
+                    Text('0.2 miles away', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600), overflow: TextOverflow.ellipsis,),
+                  ],
+                )
+              ),
+            )
+          ],
+        )
+      ));
+    });
+
+    return Column(children: _restaurantList);
+  }
+
 
 
 
@@ -20,7 +74,7 @@ class _HomeScreenState extends State<HomeScreen> {
           title: const Text("Food Delivery", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white)),
           centerTitle: true,
           actions: <Widget>[
-            TextButton(onPressed: () {}, child: Text("Carts(0)", style:  TextStyle(color:Colors.white, fontSize: 20.0),),
+            TextButton(onPressed: () {}, child: Text("Carts(${currentUser.cart.length})", style:  TextStyle(color:Colors.white, fontSize: 20.0),),
 
             ),
           ]
@@ -50,7 +104,23 @@ class _HomeScreenState extends State<HomeScreen> {
                   hintText: "Search Food or Restaurant"),
             ),
           ),
+          RecentOrders(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Nearby Restaurants",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.2,
+                ),
 
+              ),
+
+              _buildRestaurants()
+            ]
+          )
         ],
       ),
 
--- lib/widgets/recent_orders.dart
+++ lib/widgets/recent_orders.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_ui_tutorial/data/data.dart';
 
-class RecentOrder extends StatelessWidget {
-  const RecentOrder({super.key});
+import '../models/order.dart';
+
+class RecentOrders extends StatelessWidget {
+  const RecentOrders({super.key});
+
+  Container _buildRecentOrder(BuildContext context, Order order) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      width: 320.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        border: Border.all(width: 1.0, color: Colors.grey[200]!),
+      ),
+      child: Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+
+            // Image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                image: AssetImage(order.food.imageUrl),
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            // Description
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(order.food.name + "as wwfwefwef wewefwefwefwef", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis,),
+                    SizedBox(height: 4.0),
+                    Text(order.restaurant.name, style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600), overflow:  TextOverflow.ellipsis),
+                    SizedBox(height: 4.0),
+                    Text(order.date, style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400), overflow:  TextOverflow.ellipsis),
+                    SizedBox(height: 0.0),
+
+
+                  ],
+                ),
+              ),
+            ),
+            // add insert button
+            Container(
+              margin: EdgeInsets.only(right: 20.0),
+              width: 48.0,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                color: Colors.white,
+                onPressed: () {},
+            )
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text(
+            'Recent Orders',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.2,
+            ),
+          ),
+        ),
+        Container(
+          height: 120.0,
+          // color: Colors.blue,
+          child: ListView.builder(
+            padding: EdgeInsets.only(left: 10.0)  ,
+            scrollDirection: Axis.horizontal,
+            itemCount: currentUser.orders.length,
+            itemBuilder: (BuildContext context, int index) {
+              Order order = currentUser.orders[index];
+
+              return _buildRecentOrder(context, order);
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
