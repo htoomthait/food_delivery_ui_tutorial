8fcef8c2dfca115fe04b046e0425e626ff6d8817
Htoo Maung Thait <htoomaungthait@gmail.com>
2026-04-13 17:54:01 +0630

    fix update done for add button

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -14,74 +14,79 @@ class RestaurantScreen extends StatefulWidget {
 
 class _RestaurantScreenState extends State<RestaurantScreen> {
   _buildMenuItem(Food menuItem) {
-    return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        Container(
-          height: 175.0,
-          width: 175.0,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(menuItem.imageUrl),
-              fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(menuItem.imageUrl),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(15.0),
             ),
-            borderRadius: BorderRadius.circular(15.0),
           ),
-        ),
 
-        Container(
-          height: 175.0,
-          width: 175.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Colors.black.withAlpha(85),
-                Colors.black87.withAlpha(85),
-                Colors.black54.withAlpha(85),
-                Colors.black38.withAlpha(85)
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.black.withAlpha(85),
+                  Colors.black87.withAlpha(85),
+                  Colors.black54.withAlpha(85),
+                  Colors.black38.withAlpha(85)
 
+                ],
+                stops: [
+                  0.1,
+                  0.4,
+                  0.6,
+                  0.9
+                ]
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 65.0,
+            child: Column(
+              children: <Widget>[
+
+                Text(menuItem.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.white, letterSpacing: 1.2)),
+                Text('\$${menuItem.price}', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Colors.white, letterSpacing: 1.2))
               ],
-              stops: [
-                0.1,
-                0.4,
-                0.6,
-                0.9
-              ]
             ),
           ),
-        ),
-        Positioned(
-          bottom: 65.0,
-          child: Column(
-            children: <Widget>[
 
-              Text(menuItem.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.white, letterSpacing: 1.2)),
-              Text('\$${menuItem.price}', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Colors.white, letterSpacing: 1.2))
-            ],
-          ),
-        ),
-        
-        Positioned(
-            bottom: 30.0,
-            right: 30.0,
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: IconButton(
-                  icon: Icon(Icons.add),
-                  iconSize: 30.0,
-                  color: Colors.white,
-                  onPressed: () {},
-                )
-            )
-        )
+          Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.add),
+                    iconSize: 30.0,
+                    color: Colors.white,
+                    onPressed: () {},
+                  )
+              )
+          )
 
-      ],
+        ],
+      ),
     );
   }
 
